4addb3b9cf0899229cd5f3f66f12834da01b29ef
Jey <jeremyalder@hotmail.fr>
2021-10-01 13:22:38 +0200

    style for 404 page (responsive)

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -2,14 +2,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+//CSS
+import '../../styles/404.scss';
+
 class ErrorPage extends React.Component {
 	render() {
 		return (
-			<main>
-				<span>Page 404</span>
-				<p>Oups, la page que vous demandez n'existe pas</p>
+			<main className="errorPage">
+				<h1 className="errorPage__title">404</h1>
+				<p className="errorPage__content">Oups, la page que vous demandez n'existe pas</p>
 				<Link to="/">
-					<p>Retourner sur la page d'accueil</p>
+					<p className="errorPage__link">Retourner sur la page d'accueil</p>
 				</Link>
 			</main>
 		);
